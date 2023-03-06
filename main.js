fed82c14e14dacf474f543898a3586ed2f153a4c
Igor Czyż <igorczyz13@gmail.com>
2023-03-06 20:55:54 +0100

    Bug

---
+++ main.js
@@ -0,0 +1,40 @@
+const sizeUp = document.querySelector('.sizeUp');
+const sizeDown = document.querySelector('.sizeDown');
+const colorBtn = document.querySelector('.color');
+const p = document.querySelector('p');
+
+let fontSize = 36
+
+const increase = () => {
+    fontSize += 5
+    p.style.fontSize = fontSize + 'px'
+}
+
+const decrease = () => {
+    if (fontSize <= 21) {
+        return
+    }
+    fontSize -= 5
+    p.style.fontSize = fontSize + 'px'
+}
+
+sizeUp.addEventListener('click', increase)
+sizeDown.addEventListener('click', decrease)
+
+
+//
+
+fetch("https://api.spacexdata.com/v4/404")
+    .then((response) => {
+        return response.json()
+    })
+    .then((json) => {
+        console.log(json)
+    })
+    .catch((error) => {
+        if (error.status === 404) {
+            return console.error("Page not found")
+        }
+
+        return console.error(error)
+    })
